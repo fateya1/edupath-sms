660778d78ddbe69049ae4130ef7ef6879c30f4f7
RMjomba <youremail@example.com>
2026-01-08 13:52:14 +0300

    chore: remove JS duplicates and normalize TS-only frontend structure

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,19 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
-import { AuthProvider } from "./context/AuthContext";
-import { ToastProvider } from "./context/ToastContext";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ToastProvider>
-      <AuthProvider>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </AuthProvider>
-    </ToastProvider>
+    <App />
   </React.StrictMode>
 );
